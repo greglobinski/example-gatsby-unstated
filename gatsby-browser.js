fb232660c97ae8ddf898fcac778cc40fa21c3c3d
Wendell Keuneman <wendell@keuneman.com>
2018-11-21 20:03:29 +1100

    Updated to Gatsby v2 Browser APIs
    See https://www.gatsbyjs.org/docs/browser-apis/ for more info.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,6 @@
 import React from 'react'
-import { Router } from 'react-router-dom'
 import { Provider } from 'unstated'
 
-export const replaceRouterComponent = ({ history }) => {
-  const ConnectedRouterWrapper = ({ children }) => (
-    <Provider>
-      <Router history={history}>{children}</Router>
-    </Provider>
-  )
-
-  return ConnectedRouterWrapper
+export const wrapRootElement = ({ element }) => {
+  return <Provider>{element}</Provider>
 }
